b3f7e9e6205cf8a1e0ad5b87963cd532acfe4922
Caio Ramos <aramoscaio@gmail.com>
2023-06-29 23:45:37 -0300

    feat: added dashboard view

--- video_conference/videoconf_app/views.py
+++ video_conference/videoconf_app/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import  authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'login.html', {'success': 'User created successfully'})
+        
+        else:
+            error_message = form.errors.as_text()
+            return render(request, 'register.html', {'error': error_message})
+
     return render(request, 'register.html')
 
-def login(request):
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+
+        if user is None:
+            return render(request, "login.html", {'error': 'Invalid credentials'})
+        login(request, user)
+        return redirect("/dashboard")
+
     return render(request, 'login.html')
+    
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
     
